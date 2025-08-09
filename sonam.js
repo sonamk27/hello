2a14f289ade7d3788ddc0b2eeaa8defada36b62f
sonamk27 <sonamkorade25@gmail.com>
2025-08-10 02:07:28 +0530

    Add .gitignore file

--- sonam.js
+++ sonam.js
@@ -1,2 +1,4 @@
 console.log("Hello, Sonam!");
 console.log("This is a JavaScript file named sonam.js.");
+console.log("It has been recently edited.");
+console.log("Make sure to check the .env and .gitignore files for any changes.");
